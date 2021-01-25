e601dbf21bdd23863f08a1c78bf433934162c375
amine572 <amine.mansouri111@gmail.com>
2021-01-25 23:49:06 +0100

    committ

---
+++ bin/src/main/java/tn/rnu/isi/service/CategorieService.java
@@ -0,0 +1,25 @@
+package tn.rnu.isi.service;
+
+import java.util.List;
+
+import tn.rnu.isi.model.Categorie;
+
+ 
+
+
+public interface CategorieService {
+
+	public Long save (Categorie categorie) throws Exception ;
+	
+	List<Categorie> getAll();
+ 
+	Categorie getByIdCateg(Long idCateg) throws Exception;
+	
+	int updateId (Long idCateg);
+	
+  	int updateDesig (String codeCateg, String libelleCateg, Long idCateg); 
+  	
+  	void deleteCategorie(Long idCateg);
+  	
+   
+}
